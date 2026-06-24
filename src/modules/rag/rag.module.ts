c56b13e7eb29acc191f9d4984790f59c16a81414
Muhammad-Irfan01 <irfan.bvoir@gmail.com>
2026-06-24 20:17:41 +0500

    import error fix

--- src/modules/rag/rag.module.ts
+++ src/modules/rag/rag.module.ts
@@ -6,11 +6,12 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { EmbeddingModule } from '../embedding/embedding.module';
 import { RetrivalService } from './services/retrieval.service';
+import { EmbeddingService } from '../embedding/service/embedding.service';
 
 @Module({
   imports: [EmbeddingModule, PrismaModule],
   controllers: [RagController],
-  providers: [RagService, RetrivalService, PromptBuilderService],
+  providers: [RagService, RetrivalService, PromptBuilderService, EmbeddingService],
   exports: [RagService]
 })
 export class RagModule {}

37833bc19a8b4e1e8cdbd0c2c1f50b9723cea9a0
Muhammad-Irfan01 <irfan.bvoir@gmail.com>
2026-08-14 14:30:56 +0500

    add comment

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
+// force rebuild
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
